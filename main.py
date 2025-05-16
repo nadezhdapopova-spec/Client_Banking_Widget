845f7bcb3c9b4e9fe23daa89e66ea385d3f9bbea
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-05-16 23:28:13 +0700

    verified by linters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 
 def main_1() -> None:
