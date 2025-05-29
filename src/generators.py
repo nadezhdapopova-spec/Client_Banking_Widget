f7b43b27e55a40c8d181c4be078abb15c6254670
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-05-29 20:10:27 +0700

    add funcs transaction_descriptions(), main_7() and modified func filter_by_currency()

--- src/generators.py
+++ src/generators.py
@@ -2,9 +2,18 @@ from mypyc.crash import Iterator
 
 
 def filter_by_currency(transactions: list[dict], target_currency: str) -> Iterator:
-    """Фильтрация транзакций по заданной валюте"""
+    """Фильтрует транзакции по заданной валюте"""
 
-    filtered_transactions = (cur for cur in transactions if
-                             cur["operationAmount"]["currency"]["name"] == target_currency)
+    filtered_transactions = (desc for desc in transactions if
+                             desc["operationAmount"]["currency"]["name"] == target_currency)
 
     return filtered_transactions
+
+
+def transaction_descriptions(transactions: list[dict]) -> Iterator:
+    """Возвращает описание каждой операции транзакции по очереди"""
+
+    descriptions = (desc["description"] for desc in transactions)
+
+    for desc in descriptions:
+        yield desc
