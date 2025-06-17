38bc94bb97363b7303e546e0162fdcbbc988fed5
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-06-17 18:06:27 +0700

    created loggers for src/masks.py and src/utils.py in src/logging_config.py

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,7 @@ import os
 
 from config import ROOT_DIR
 from src.decorators import log
+from src.logging_config import masks_logger
 
 
 # @log()
@@ -14,10 +15,13 @@ def get_mask_card_number(card_number: int | str) -> str:
     card_symbols = str(card_number)
 
     if len(card_symbols) != 16:
+        masks_logger.error(f"Номер карты {card_number} имеет недопустимую длину.")
         raise ValueError("Номер карты должен содержать 16 цифр.")
     if not card_symbols.isdigit():
+        masks_logger.error(f"Номер карты {card_number} имеет недопустимые символы.")
         raise ValueError("Номер карты должен содержать только цифры.")
 
+    masks_logger.info(f"Маска для номера карты {card_number} создана.")
     return f"{card_symbols[:4]} {card_symbols[4:6]}** **** {card_symbols[-4:]}"
 
 
@@ -30,9 +34,12 @@ def get_mask_account(account_number: int | str) -> str:
     account_symbols = str(account_number)
 
     if len(account_symbols) < 4:
+        masks_logger.error(f"Номер счёта {account_number} имеет недопустимую длину.")
         raise ValueError("Номер счёта должен содержать не менее 4 цифр.")
 
     if not account_symbols.isdigit():
+        masks_logger.error(f"Номер счёта {account_number} имеет недопустимые символы.")
         raise ValueError("Номер счета должен содержать только цифры.")
 
+    masks_logger.info(f"Маска для номера счёта {account_number} создана.")
     return f"**{account_symbols[-4:]}"
